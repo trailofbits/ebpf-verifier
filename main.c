2252c5573484fdc2ef2d813553309181d0a5059a
baumanl <laurabauman12@gmail.com>
2022-06-22 17:42:51 -0700

    added a prelim call to bpf_check from main

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int printf(const char *, ...);
+extern int bpf_check(void * a, void * b, void * c);
 
 int main() {
   printf("hello world\n");
+  bpf_check(NULL, NULL, NULL);
   return 0;
 }
